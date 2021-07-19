d135c12b15439e98d9655705f65c2225dde4ae72
AleksRu21 <sasharudyak92@gmail.com>
2021-07-19 15:13:17 +0200

    Created new app

---
+++ src/weather.js
@@ -0,0 +1,15 @@
+import logo from "./logo.svg";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <form onClick>
+        <input type="search" autoFocus="on" />
+        <input type="button" value="Search" />
+      </form>
+    </div>
+  );
+}
+
+export default App;
