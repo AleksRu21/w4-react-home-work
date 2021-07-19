a69493eddf2ce7f4bd4372ba2acec5f5465bdc77
AleksRu21 <sasharudyak92@gmail.com>
2021-07-19 17:38:13 +0200

    changing colour, adding account

--- src/weather.js
+++ src/weather.js
@@ -2,19 +2,29 @@ import "./App.css";
 
 function App() {
   return (
-    <body>
-      <div className="container">
-        <h1>Weather Forecast</h1>
-        <form onClick>
-          <input type="search" autoFocus="on" />
-          <input type="button" value="Search" />
-        </form>
-        <h2>Ede</h2>
-      </div>
+    <div className="container">
+      <h1>Weather Forecast</h1>
+      <form onClick>
+        <input type="search" autoFocus="on" />
+        <input type="button" value="Search" />
+      </form>
+      <h2>Ede</h2>
+      <br />
+      <i className="fas fa-cloud-sun"></i>
       <footer>
-        <a source>Open-source code</a>, by <a>Aleksandra Rudyak</a>
+        <a
+          href="https://github.com/AleksRu21/w4-react-home-work"
+          target="_blank"
+          rel="noreferrer"
+        >
+          Open-source code
+        </a>
+        , by{" "}
+        <a href="https://www.linkedin.com/in/aleksandra-rudyak/">
+          Aleksandra Rudyak
+        </a>
       </footer>
-    </body>
+    </div>
   );
 }
 
